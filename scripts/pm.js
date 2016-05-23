92f5f106675522f71b1fc2e00113f8fe6f082d5a
Jonathan Arnett <jonarnett90@gmail.com>
2016-05-23 14:50:44 -0400

    Added timestamp and coffee

--- scripts/pm.js
+++ scripts/pm.js
@@ -16,8 +16,41 @@
 'use strict';
 
 module.exports = (robot) => {
+    const yes = (str) => {
+        const lowerStr = str.toLowerCase();
+        return lowerStr === "yes" || lowerStr === "y" || lowerStr === "sure";
+    }
+
+    const private_message = (msg) => {
+        return msg.message.room === undefined;
+    }
+
     robot.respond(/pm me/, (msg) => {
-        robot.send({user: {name:  msg.message.user.name}}, "HI!");
+        const today = new Date();
+        msg.sendPrivate(`Hi! The time is currently ${today.toISOString()}.`);
+        msg.sendPrivate("Would you like a newspaper and coffee?");
+
+        robot.brain.set('coffee_recipient', msg.message.user.name);
+    });
+
+    robot.respond(/(.*)/, (msg) => {
+        if (private_message(msg) && msg.message.user.name === robot.brain.get('coffee_recipient')) {
+            if (yes(msg.match[1])) {
+                msg.send('+------------+');
+                msg.send('| THE TIMES  |');
+                msg.send('| ~ ~~~ ~~ ~ |');
+                msg.send('| ~ ~~~ ~~ ~ |');
+                msg.send('\\ ~ ~~~ ~~ ~ \\     ____');
+                msg.send(' | ~ ~~~ ~~ ~ |   /____\\');
+                msg.send(' | ~ ~~~ ~~ ~ |   |____|');
+                msg.send(' | ~ ~~~ ~~ ~ |   |____|');
+                msg.send(' +------------+    \\__/');
+            } else {
+                msg.send('OK then');
+            }
+
+            robot.brain.set('coffee_recipient', '');
+        }
     });
 }
 
