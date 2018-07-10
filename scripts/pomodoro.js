aa7d67854e18fb7844936d8fe4158a510349191b
Jonathan Arnett <jarnett@covermymeds.com>
2018-07-10 11:32:20 -0400

    Add pomodoro script

---
+++ scripts/pomodoro.js
@@ -0,0 +1,61 @@
+// Description:
+//   Have Robot Overlord orchestrate group Pomodoro sessions
+//
+// Dependencies:
+//
+// Configuration:
+//
+// Commands:
+//   @covermybot start timer
+//   @covermybot stop timer
+//
+// Notes:
+//
+// Author:
+//   J3RN
+
+module.exports = (robot) => {
+    var twenty_five_minutes = 25 * 60 * 1000;
+    var fifteen_minutes = 15 * 60 * 1000;
+    var five_minutes = 5 * 60 * 1000;
+
+    var pomodoroEnabled = false;
+    var pomodoroCount = 0;
+    var timeout = null;
+
+    function workNotify(msg) {
+	msg.send("Work time!");
+	msg.send("Reply 'stop timer' to stop.");
+	timeout = setTimeout(breakNotify, twenty_five_minutes, msg);
+    }
+
+    function breakNotify(msg) {
+	msg.send("Break time!");
+	msg.send("Reply 'stop timer' to stop.");
+	pomodoroCount++;
+	if (pomodoroCount % 4 == 0) {
+	    timeout = setTimeout(workNotify, fifteen_minutes, msg);
+	} else {
+	    timeout = setTimeout(workNotify, five_minutes, msg);
+	}
+    }
+
+    robot.respond(/start timer/i, (msg) => {
+	if (pomodoroEnabled) {
+	    msg.send("We're already working here!");
+	} else {
+	    pomodoroEnabled = true;
+	    workNotify(msg);
+	}
+    });
+
+    robot.respond(/stop timer/i, (msg) => {
+	if (pomodoroEnabled) {
+	    pomodoroEnabled = false;
+	    clearTimeout(timeout);
+	    msg.send("Timer stopped!");
+	} else {
+	    msg.send("Who's working? We're not working.");
+	}
+    });
+}
